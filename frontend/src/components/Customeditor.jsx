13f68d81265926566aeaf6bbf854108b430b23e7
Sayanchowdhury123 <sayanchoudhury506@gmail.com>
2025-07-28 22:49:39 +0530

    text editor

--- frontend/src/components/Customeditor.jsx
+++ frontend/src/components/Customeditor.jsx
@@ -1,34 +1,97 @@
-import { EditorContent, useEditor } from "@tiptap/react"
+import { EditorContent, EditorContext, useEditor } from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
 import { useEffect, useState } from "react"
+import { FaBold } from "react-icons/fa";
+import { FaItalic } from "react-icons/fa";
+import { FaStrikethrough } from "react-icons/fa";
+import { FaCode } from "react-icons/fa";
+import { FaUnderline } from "react-icons/fa";
+import { BsBlockquoteRight } from "react-icons/bs";
+import { BlockquoteButton } from "./tiptap-ui/blockquote-button";
+import { ToolbarGroup } from "./tiptap-ui-primitive/toolbar";
+import { HeadingDropdownMenu } from "./tiptap-ui/heading-dropdown-menu";
+import { Toolbar } from "./tiptap-ui-primitive/toolbar";
 
-
-
-export default function Customeditor({intialContent="",onContentChange}) {
-  const [content,setcontent] = useState(intialContent)
+export default function Customeditor({ intialContent = "", onContentChange }) {
+  const [content, setcontent] = useState(intialContent)
+  const [toggle, settoggle] = useState(true)
 
   const editor = useEditor({
     extensions: [StarterKit],
     content: intialContent,
-    onUpdate: ({editor}) => {
-        const html = editor.getHTML()
-        setcontent(html)
-        if(onContentChange){
-            onContentChange(html)
-        }
+    onUpdate: ({ editor }) => {
+      const html = editor.getHTML()
+      setcontent(html)
+      if (onContentChange) {
+        onContentChange(html)
+      }
     }
   })
 
 
   useEffect(() => {
-    if(editor && intialContent !== editor.getHTML() && intialContent){
-        editor.commands.setContent(intialContent)
+    if (editor && intialContent !== editor.getHTML() && intialContent) {
+      editor.commands.setContent(intialContent)
     }
-  },[intialContent,editor])
+  }, [intialContent, editor])
 
-    return(
-        <div>
-          <EditorContent editor={editor} />
-        </div>
+  function Toolbar1() {
+    if (!editor) return null;
+    return (
+      <div className="flex gap-3 mb-4">
+        <button type="button" onClick={() => {
+          editor.chain().focus().toggleBold().run()
+        }} className={editor.isActive("bold") ? "font-bold" : ""} >
+          <FaBold/>
+        </button>
+
+        <button type="button" onClick={() => {
+          editor.chain().focus().toggleItalic().run()
+          settoggle((prev) => !prev)
+        }} >
+          <FaItalic />
+        </button>
+
+        <button type="button" onClick={() => {
+          editor.chain().focus().toggleStrike().run()
+          settoggle((prev) => !prev)
+        }} >
+          <FaStrikethrough />
+        </button>
+
+
+        <button type="button" onClick={() => {
+          editor.chain().focus().toggleCode().run()
+          settoggle((prev) => !prev)
+        }} >
+          <FaCode />
+        </button>
+
+        <button type="button" onClick={() => {
+          editor.chain().focus().toggleUnderline().run()
+          settoggle((prev) => !prev)
+        }} >
+          <FaUnderline />
+        </button>
+
+
+
+
+
+
+      </div>
     )
+  }
+
+  return (
+    <EditorContext.Provider value={{editor}}>
+      <div className="tiptap-toolbar-container">
+      <Toolbar1/>
+      </div>
+     
+      <EditorContent editor={editor} className="tiptap-editor-content" />
+
+
+    </EditorContext.Provider>
+  )
 }
--- frontend/src/pages/Editcontent.jsx
+++ frontend/src/pages/Editcontent.jsx
@@ -1,39 +1,91 @@
 import Customeditor from '@/components/Customeditor'
+import Sidebar from '@/components/Sidebar'
 import { SimpleEditor } from '@/components/tiptap-templates/simple/simple-editor'
 import useAuthstore from '@/store/authstore'
 import { useEffect, useRef, useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import RichTextEditor from 'reactjs-tiptap-editor';
+import { BaseKit } from 'reactjs-tiptap-editor';
+import 'reactjs-tiptap-editor/style.css';
+import { Blockquote } from 'reactjs-tiptap-editor/blockquote';
+import { Bold } from 'reactjs-tiptap-editor/bold';
+import { CodeBlock } from 'reactjs-tiptap-editor/codeblock';
+import 'prism-code-editor-lightweight/layout.css';
+import 'prism-code-editor-lightweight/themes/github-dark.css';
+import { BulletList } from 'reactjs-tiptap-editor/bulletlist';
+import { Color } from 'reactjs-tiptap-editor/color';
+import { Clear } from 'reactjs-tiptap-editor/clear';
+import { Code } from 'reactjs-tiptap-editor/code';
+import { Document } from 'reactjs-tiptap-editor/document';
+import { Strike } from 'reactjs-tiptap-editor/strike';
+import { Heading } from 'reactjs-tiptap-editor/heading';
+import { Highlight } from 'reactjs-tiptap-editor/highlight';
+import { ListItem } from 'reactjs-tiptap-editor/listitem';
+import { Link } from 'reactjs-tiptap-editor/link';
+import { FontFamily } from 'reactjs-tiptap-editor/fontfamily';
+import { FontSize } from 'reactjs-tiptap-editor/fontsize';
+import { Selection } from 'reactjs-tiptap-editor/selection';
+import { Italic } from 'reactjs-tiptap-editor/italic';
+import { TextAlign } from 'reactjs-tiptap-editor/textalign';
+import { SubAndSuperScript } from 'reactjs-tiptap-editor/subandsuperscript';
+import { SlashCommand } from 'reactjs-tiptap-editor/slashcommand';
+import { TextUnderline } from 'reactjs-tiptap-editor/textunderline';
+import { OrderedList } from 'reactjs-tiptap-editor/orderedlist';
+import { LineHeight } from 'reactjs-tiptap-editor/lineheight';
+
+const extensions = [
+  BaseKit, Blockquote, Bold, CodeBlock, BulletList, Color, Clear, Code, Document, Strike, Heading, Highlight, ListItem, Link, FontFamily, FontSize, Selection, Italic, TextAlign, SubAndSuperScript, SlashCommand, TextUnderline, OrderedList, LineHeight
+
+
+]
 
 export default function Editcontent() {
- const location = useLocation()
-const [blogtext,setblogtext] = useState(location.state?.t || "")
+  const location = useLocation()
+  const [blogtext, setblogtext] = useState(location.state?.t || "")
+
+  const { setshownav } = useAuthstore()
 
 
 
 
 
 
-
-
-const handlesubmit = (e) => {
+  const handlesubmit = (e) => {
     e.preventDefault()
-    
+
     const formdata = new FormData()
     formdata.append("blogtext", blogtext)
     console.log(blogtext);
-    
-    
-}
 
-  return(
-    <div >
-      <form onSubmit={handlesubmit} >
-        <Customeditor intialContent={blogtext} onContentChange={setblogtext}/>
-        <button type='submit'>Submit</button>
-      </form>
-      
+
+  }
+
+  return (
+    <div className="  relative  overflow-x-hidden">
+      <Sidebar />
+      <div className='p-4 space-y-4'>
+        <div className="flex items-center justify-between " >
+          <h1 className="text-4xl font-bold">  BlogApp</h1>
+          <img src="jj" alt="img" className="w-8 h-8 bg-black rounded-full" onClick={setshownav} />
+        </div>
+
+        <div>
+          <h1 className="text-3xl text-center font-semibold">Edit Content</h1>
+        </div>
+
+        <div className='flex justify-center items-center   '>
+          <form onSubmit={handlesubmit} className="w-[984px]" >
+            <RichTextEditor extensions={extensions} output='html' content={blogtext} onChangeContent={setblogtext}  />
+            <button type='submit' className='btn btn-neutral mt-2'>Submit</button>
+          </form>
+
+        </div>
+
+      </div>
+
+
 
     </div>
- 
-  ) 
+
+  )
 }
